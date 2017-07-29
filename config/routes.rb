c25a45e60726dab6c3ea01433d122f16d3baaa37
Raoul D'Cunha <raoul_dcunha@hotmail.com>
2017-07-29 21:14:06 +1200

    CORS Plssssssssssssssssssssssss

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'welcome#index'
     resources :volunteers, :login, :skills, :job, :charity, :sector
+    match '*any' => 'application#options', :via => [:options]
 end
