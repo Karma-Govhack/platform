5628ab5c11ca02c3a55ec8f87b298fbbe88a2110
Raoul D'Cunha <raoul_dcunha@hotmail.com>
2017-07-29 20:03:59 +1200

    added support for filtering jobs that are nearby

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -1,9 +1,13 @@
 class JobController < ApplicationController
   before_action :set_job, only: [:show, :update, :destroy]
 
-  # GET /job
+  # GET /job?user_id=?
   def index
-    @job = Job.all
+    user_id = params[:user_id]
+    volunteer = Volunteer.find(user_id)
+    @job = Job.all.select do |j|
+      getDistanceFromLatLonInKm(j.latitude, j.longtitude, volunteer.latitude, volunteer.longtitude) < 10
+    end
     render json: @job, status: :ok
   end
 
@@ -39,6 +43,23 @@ class JobController < ApplicationController
 
   def set_job
     @job = Job.find(params[:id])
+  end
+
+  def getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2)
+    r = 6371 #Radius of the earth in km
+    dLat = deg2rad(lat2-lat1)  #deg2rad below
+    dLon = deg2rad(lon2-lon1)
+    a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    R * c #Distance in km
+  end
+
+  def deg2rad(deg)
+    deg * (Math.PI/180)
   end
 
 end
