1c7111759087ca6a7d7d37066dda99a0fd9d23dc
Raoul D'Cunha <raoul_dcunha@hotmail.com>
2017-07-29 20:18:00 +1200

    fixed syntax errors

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -46,16 +46,15 @@ class JobController < ApplicationController
   end
 
   def getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2)
-    r = 6371 #Radius of the earth in km
+    earth_radius = 6371 #Radius of the earth in km
     dLat = deg2rad(lat2-lat1)  #deg2rad below
     dLon = deg2rad(lon2-lon1)
     a = 
       Math.sin(dLat/2) * Math.sin(dLat/2) +
       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2)
-      ; 
+      Math.sin(dLon/2) * Math.sin(dLon/2) 
     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-    R * c #Distance in km
+    earth_radius * c #Distance in km
   end
 
   def deg2rad(deg)
