063be6c8f007b52ab794997f49a53ca2b21bf42b
Raoul D'Cunha <raoul_dcunha@hotmail.com>
2017-07-30 14:09:34 +1200

    and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'welcome#index'
-    resources :volunteers, :login, :skills, :job, :charity, :sector
+    resources :volunteers, :login, :skills, :job, :charityapi, :sector
     match '*any' => 'application#options', :via => [:options]
 end
